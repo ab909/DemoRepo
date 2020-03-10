7e43db5d9e222ee1ee8579813e83211d4f7d1403
Abhi Bhatt <abhi.bhatt100@gmail.com>
2020-03-10 15:58:57 -0400

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,11 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	public void loginPage() {
+		System.out.println("login method");
+	}
+	
+	public void loginSet() {
+		System.out.println("loginSet method");
+	}
 }
